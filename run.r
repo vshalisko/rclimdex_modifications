892a5b48ae452e1e60c7d04eabf3b451ccd2474d
Viacheslav <vshalisko@users.noreply.github.com>
2016-03-29 17:56:58 -0600

    Example of analysis RPC45

---
+++ run.r
@@ -0,0 +1,3 @@
+#source("C:/Google_Drive/Projects_programming/R_working/RClimdex/rclimdex_modification_sp.r") 
+source("C:/Google_Drive/Projects_programming/R_working/RClimdex/rclimdex_tmedia_sp.r") 
+
